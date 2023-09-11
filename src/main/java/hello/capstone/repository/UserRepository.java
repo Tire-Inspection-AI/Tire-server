a9acd5d6b4f6e92c2dcf94fff03a3c5aafe8a167
Hoo-sung <gntjd135@gmail.com>
2023-09-11 10:45:36 +0900

    Feat: User entity, User Repository 생성

---
+++ src/main/java/hello/capstone/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package hello.capstone.repository;
+
+
+import hello.capstone.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    public Optional<User> findById(Long id);
+
+    public Optional<User> findByUsername(String username);
+}
